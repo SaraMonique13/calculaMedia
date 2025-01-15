99bd28045700ccdbbcee6914e6dd30feaa803ef4
Sara <moniquesarah5@gmail.com>
2025-01-15 17:46:58 -0300

    media

--- main.js
+++ main.js
@@ -42,5 +42,10 @@ function atualizaTabela(){
 }
 
 function atualizaMediaFinal(){
+    let soma = 0
 
+    for(let i = 0; i < notas.length; i++){
+        soma += notas[i]
+    }
+    const media = 
 }
